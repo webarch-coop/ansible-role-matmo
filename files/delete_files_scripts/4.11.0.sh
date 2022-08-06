7e339f6537fa5c533d9bede9874fdb2ca075935a
Chris Croome <chris@webarchitects.co.uk>
2022-08-06 12:25:06 +0100

    Updates for Matomo 4.11.0

---
+++ files/delete_files_scripts/4.11.0.sh
@@ -0,0 +1,40 @@
+#!/bin/bash
+
+set -e
+
+# DIRS=""
+FILES="piwik.min.js
+plugins/SEO/Metric/Alexa.php"
+# SYMLINKS=""
+
+# Delete files
+for f in ${FILES}; do
+  if [[ -f "${f}" ]]; then
+    echo "Deleting file ${f}"
+    rm "${f}"
+  else
+    echo "File ${f} has already been deleted"
+  fi
+done
+
+# Delete symlinks
+# for s in ${SYMLINKS}; do
+#   if [[ -L "${s}" ]]; then
+#     echo "Deleting symlink ${s}"
+#     rm "${s}"
+#   else
+#     echo "Symlink ${s} has already been deleted"
+#   fi
+# done
+
+# Delete directories
+# for d in ${DIRS}; do
+#   if [[ -d "${d}" ]]; then
+#     echo "Deleting directory ${d}"
+#     rm -rf "${d}"
+#   else
+#     echo "Directory ${d} has already been deleted"
+#   fi
+# done
+
+exit 0
